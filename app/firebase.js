f87b15595800e84fbfd52b76f243b919c2c29204
Rajani Khatri <rajanikhatri8876@gmail.com>
2024-08-23 15:01:29 -0400

    Fix router

--- app/firebase.js
+++ app/firebase.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, signInWithRedire
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCWHZgJCNjebBL_WrZJ7ptAtN5XqytrSiM",
-    authDomain: "chat-bot-88f14.firebaseapp.com",
-    projectId: "chat-bot-88f14",
-    storageBucket: "chat-bot-88f14.appspot.com",
-    messagingSenderId: "152667588748",
-    appId: "1:152667588748:web:6a4afae1c474b15f5b13a9",
-    measurementId: "G-1JYQ5JYRHW"
+    apiKey: "process.env.APIKEY",
+    authDomain: "process.env.AUTHDOMAIN",
+    projectId: "pantrytracker01",
+    storageBucket: "pantrytracker01.appspot.com",
+    messagingSenderId: "87648405047",
+    appId: "1:87648405047:web:bbd4c96d7542419bc361b2",
+    measurementId: "G-DSZ5Y2E3NY"
 };
 
 // Initialize Firebase
